b37185fe17dba4cf9ee67485cf2632edc17f334f
Daksh <dakshjkhadalpur177@gmail.com>
2025-03-12 00:23:02 +0530

    updaed sinner and fixed issues

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -2,17 +2,25 @@ import { AppContext } from "../Context/AppContext";
 import Spinner from "./Spinner";
 import { useContext } from "react";
 import Cards from "./Cards";
+
 function Blogs() {
-  const { loading,posts } = useContext(AppContext);
-console.log(posts);
+  const { loading, posts } = useContext(AppContext);
+  
   return (
     <div className="h-[80vh] overflow-scroll">
-      {
-      loading ? ("waaiting") : (posts.length===0 ? (<div><p>post not available</p></div>):(posts.map( (post) =><Cards key={post.id} post={post}></Cards>)))
-      }
-  
-      
+      {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <Spinner />
+        </div>
+      ) : posts.length === 0 ? (
+        <div className="text-center">
+          <p>Post not available</p>
+        </div>
+      ) : (
+        posts.map((post) => <Cards key={post.id} post={post} />)
+      )}
     </div>
   );
 }
+
 export default Blogs;
--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -1,8 +1,13 @@
-function Spinner(){
-  return(
-    <div className="Spinner">
-      <img src="./images/giphy.gif" alt="" />
+function Spinner() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <img
+        src="https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif" 
+        alt="Waiting..."
+        className="w-50 h-50"
+      />
     </div>
-  )
+  );
 }
+
 export default Spinner;
